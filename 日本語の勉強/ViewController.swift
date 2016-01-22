6d40aa0d258c6b8c2e75bb1546cff48fcc07d56d
cihai sun <suncihai@gmail.com>
2016-01-22 14:44:28 -0500

    Initial Commit

---
+++ 日本語の勉強/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  日本語の勉強
+//
+//  Created by suncihai on 16/1/22.
+//  Copyright © 2016年 suncihai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
